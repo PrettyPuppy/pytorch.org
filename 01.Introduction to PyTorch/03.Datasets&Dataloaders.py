a2e0cdd4d15c33f96f38049c06be9724d76a4d39
Happy Smile <happysmiler@gmail.com>
2023-07-15 18:05:53 -0700

    update dataset & dataloader

--- 01.Introduction to PyTorch/03.Datasets&Dataloaders.py
+++ 01.Introduction to PyTorch/03.Datasets&Dataloaders.py
@@ -42,3 +42,45 @@ for i in range(1, cols * rows + 1):
     plt.imshow(img.squeeze(), cmap='gray')
     
 plt.show()
+
+
+#
+# Creating custom dataset for your files
+#
+
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotation_file, img_dir, transform=None, target_transform=None) -> None:
+        self.img_labels = pd.read_csv(annotation_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+    
+    def __len__(self):
+        return len(self.img_labels)
+    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+#   
+# Preparing your data for training with DataLoaders
+#
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+#
+# Iterate through the DataLoader
+#
